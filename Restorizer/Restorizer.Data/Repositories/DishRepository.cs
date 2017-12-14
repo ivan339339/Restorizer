079d423b9099ba6485d2b09b3a5a1fb00744e344
Boris Neklyudov <boris-1999@mail.ru>
2017-12-14 22:10:27 +0300

    Basic LINQ Queries

--- Restorizer/Restorizer.Data/Repositories/DishRepository.cs
+++ Restorizer/Restorizer.Data/Repositories/DishRepository.cs
@@ -11,5 +11,26 @@ namespace Restorizer.Data.Repositories
     class DishRepository : Repository<Dish>, IDishRepository
     {
         public DishRepository(Context context) : base(context) { }
+
+        public Dish GetMaxProfit()
+        {
+            var dish = new Dish();
+            var dishes = _context.Dishes.Include("Orders");
+            var dishhasorder = from d in dishes
+                               select d.Orders;
+
+
+            return dish;
+        }
+
+        public List<Dish> Get5LeastSold()
+        {
+            var dishes = _context.Dishes.Include("Orders").ToList();
+            var query = (from d in dishes
+                         select d.Orders);
+            var getorders = from q in query
+                            select q.FirstOrDefault();
+            return dishes;
+        }
     }
 }
