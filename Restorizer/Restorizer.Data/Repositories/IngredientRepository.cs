5fd64d0b7c04731cbb2a5f1023d2b9fb9fa0c590
Boris Neklyudov <boris-1999@mail.ru>
2017-12-16 16:35:58 +0300

    Created a linq query for getting 5 least popular ingredients

--- Restorizer/Restorizer.Data/Repositories/IngredientRepository.cs
+++ Restorizer/Restorizer.Data/Repositories/IngredientRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Restorizer.Data.Interfaces;
 using Restorizer.Data.Model;
+using Restorizer.Data.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,34 @@ namespace Restorizer.Data.Repositories
     class IngredientRepository : Repository<Ingredient>, IIngredientRepository
     {
         public IngredientRepository(Context context) : base(context) { }
+
+        public List<IngredientTotalAmount> Get5LeastPopularIngredient()
+        {
+            var AllIngredients = _context.Ingredients.ToList();
+            var ingredientsamount = from ai in AllIngredients
+                                    select ai.Dishes;
+
+            List<DishHasIngredient> ingredients = ingredientsamount.SelectMany(x => x).ToList();
+
+            List<IngredientTotalAmount> ingredientstotalamount = new List<IngredientTotalAmount>();
+
+            var groupedingredient = from i in ingredients
+                                   group i by i.Ingredient;
+            foreach (var item in groupedingredient)
+            {
+                int totalamount = 0;
+                foreach (var item1 in item)
+                {
+                    totalamount += item1.AmountInG;
+                }
+                ingredientstotalamount.Add(new IngredientTotalAmount { Ingr = item.Key, TotalQuantity = totalamount });
+            }
+
+            var sordtedingredient = (from ita in ingredientstotalamount
+                                    orderby ita.TotalQuantity ascending
+                                    select ita).ToList();
+
+            return new List<IngredientTotalAmount> { sordtedingredient[0], sordtedingredient[1], sordtedingredient[2], sordtedingredient[3], sordtedingredient[4] };
+        }
     }
 }
