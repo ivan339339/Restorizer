937c507086dea3d0c9ae2152197bd9b321e297c1
Ilya Gromov <ipgromov@edu.hse.ru>
2017-12-16 21:43:13 +0300

    Worked on dishes section features

--- Restorizer/Restorizer.UI/Pages/DishesPage.xaml.cs
+++ Restorizer/Restorizer.UI/Pages/DishesPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Restorizer.Data;
+using Restorizer.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,8 +22,24 @@ namespace Restorizer.UI.Pages
     /// </summary>
     public partial class DishesPage : Page, ISectionPage
     {
-
         public string Heading { get; } = "Dishes";
+
+        private Dish _selectedDish = null;
+
+        private Dish SelectedDish
+        {
+            set
+            {
+                _selectedDish = value;
+                UpdateButtons();
+            }
+        }
+
+        private void UpdateButtons()
+        {
+            var buttons = new List<Button> { EditButton, DeleteButton };
+            buttons.ForEach(btn => btn.IsEnabled = _selectedDish != null);
+        }
 
         public DishesPage()
         {
@@ -31,6 +49,42 @@ namespace Restorizer.UI.Pages
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(PagesFactory.Default.AddDishPage);
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshListView();
+            SelectedDish = null;
+        }
+
+
+        private void RefreshListView()
+        {
+            DishesListView.ItemsSource = null;
+            DishesListView.ItemsSource = LoadDishes();
+        }
+
+        private IEnumerable<object> LoadDishes()
+        {
+            IEnumerable<object> result;
+            using (var uow = new UnitOfWork())
+            {
+                result = uow.Dishes.GetByCategories();
+            }
+            return result;
+        }
+
+        private void DishView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedListView = sender as ListView;
+            if (selectedListView.SelectedIndex == -1)
+            {
+                _selectedDish = null;
+            }
+            else
+            {
+                SelectedDish = selectedListView.SelectedItem as Dish;
+            }
         }
     }
 }
